a1753bf5da9e5ef944cb06bb508aa6305ba54676
Mohamed Ahmed <43890671+mohamedmed01345@users.noreply.github.com>
2022-11-24 21:21:15 +0200

    Update check.php

--- check.php
+++ check.php
@@ -14,20 +14,23 @@ $stmt = $conn->prepare("SELECT full_name, dr FROM members WHERE card_id= ?");
 $stmt->bind_param('s',$id);
 $stmt->execute();
 $stmt->bind_result($name, $role);
-$row = $stmt->fetch();
-$stmt->close();
-if($role==1){
-    $dr_stmt = $conn->prepare("SELECT members.full_name, lectures.LECTURE_NAME, lectures.start_at, lectures.end_at FROM members, lectures WHERE members.card_id=? AND members.card_id=lectures.card_id");
-    $dr_stmt->bind_param("s",$id);
-    $dr_stmt->execute();
-    $dr_stmt->bind_result($dr_name, $lectures, $start_at, $end_at);
-    while($dr_stmt->fetch()){
-    echo "DR|".$dr_name."|".$lectures."|".$start_at."|".$end_at."<br>";
+if($stmt->fetch()){
+    $stmt->close();
+    if($role==1){
+        $dr_stmt = $conn->prepare("SELECT members.full_name, lectures.LECTURE_NAME, lectures.start_at, lectures.end_at FROM members, lectures WHERE members.card_id=? AND members.card_id=lectures.card_id");
+        $dr_stmt->bind_param("s",$id);
+        $dr_stmt->execute();
+        $dr_stmt->bind_result($dr_name, $lectures, $start_at, $end_at);
+        while($dr_stmt->fetch()){
+        echo "DR|".$dr_name."|".$lectures."|".$start_at."|".$end_at."<br>";
+        }
+        $dr_stmt->close();
     }
-    $dr_stmt->close();
-}
-elseif($role==0){
-    echo "SD|".$name;
+    elseif($role==0){
+        echo "SD|".$name;
+    }
+}else{
+    die("ID isn't in the records");
 }
 $conn->close();
 ?>
