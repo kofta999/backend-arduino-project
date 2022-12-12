343e831bc638e248e4ecec24a5efeb4b87bdda30
Mostafa Mahmoud <99273340+kofta999@users.noreply.github.com>
2022-12-12 17:53:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -15,11 +15,11 @@ try:
         record = cursor.fetchone()
         print("You're connected to database: ", record)
 
-        for i in range(1,10):
-            query = """INSERT INTO students_docs_test_table	(card_id, id, name, role) VALUES ("1q2w3e4r5t", {}, "{}", 'student')""".format(i, fake.first_name())
-            cursor.execute(query)
-            connection.commit()
-            print(cursor.rowcount, "Record inserted successfully into Laptop table")
+#         for i in range(1,10):
+#             query = """INSERT INTO students_docs_test_table	(card_id, id, name, role) VALUES ("1q2w3e4r5t", {}, "{}", 'student')""".format(i, fake.first_name())
+#             cursor.execute(query)
+#             connection.commit()
+#             print(cursor.rowcount, "Record inserted successfully into Laptop table")
 
 except Error as e:
     print("Error while connecting to MySQL", e)
